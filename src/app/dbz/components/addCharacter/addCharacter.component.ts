7ca3e861f511a53b1e04267dccc468c63e40f45e
Borja HP <bheras@ibercacao.es>
2023-11-17 17:28:27 +0100

    Modularización

---
+++ src/app/dbz/components/addCharacter/addCharacter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dbz-add-character',
+  templateUrl: './addCharacter.component.html',
+  styleUrls: ['./addCharacter.component.css'],
+
+})
+export class AddCharacterComponent { }
