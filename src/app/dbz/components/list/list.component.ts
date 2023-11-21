42db0010cc3b50ea76530d51aaa71eda7431bc20
Borja HP <bheras@ibercacao.es>
2023-11-21 17:52:03 +0100

    Avanzamos

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import {  Component } from '@angular/core';
+import {  Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -6,4 +7,12 @@ import {  Component } from '@angular/core';
   styleUrls: ['./list.component.css'],
 
 })
-export class ListComponent { }
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }];
+
+}
